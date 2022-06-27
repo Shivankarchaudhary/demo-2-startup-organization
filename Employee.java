ec53b69c453c90129dfc8ce0204f2cadef3edebb
Karen Jebakany <Karen.Jebakany@in.niit.com>
2022-06-27 18:06:35 +0530

    Demo Solution added

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee {
+    int employeeCode;
+    String employeeName;
+    int age;
+    String dob;
+    String address;
+    double salary;
+
+    double calculateSalaryHike(float hikePercentage) {
+        return 0.0;
+    }
+}
